13f5fa86839b80601ca7b66f63f5e18956aaa19f
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-11-24 23:32:47 +0100

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function getArticles() {
   let jsonArray = [];
   let articles = document.querySelectorAll('article');
+  
   articles.forEach(function (article, index) {
     let price = article.querySelector('.cijena').innerText;
     let shipping = article.querySelector('.dostava-info').innerText;
@@ -16,7 +17,7 @@ function getArticles() {
     image = image.replace(/["']/g, "");
     let quantity = article.querySelector('.tag_kolicina');
     let oldPrice = article.querySelector('.cijena-stara');
-    jsonArray.push({
+    var obj = {
       "title": title,
       "price": price,
       "shippingPrice": shipping,
@@ -24,24 +25,22 @@ function getArticles() {
       "link": link,
       "quantity": quantity != null? quantity.innerText : 'Na stanju',
       "oldPrice": oldPrice != null? oldPrice.innerText: null
-    });
+    }
+    jsonArray.push({obj});
+    addArticles(obj);
+
   })
-  document.querySelector('.filteri').innerHTML = `<h1>JSON DATA - EKSTENZIJA NAPRAVLJENA 24.11.2021.</h1><textarea readonly rows="15" cols="180" id="jsonData">${JSON.stringify(jsonArray)}</textarea>`;
-  
-  deleteArticles();
-  setTimeout(function(){
-    addArticles(jsonArray);
-  }, 1000)
+  document.querySelector('.filteri').innerHTML = `<h2>USPJESNO STE IZBRISALI STARE I DODALI NOVE ARTIKLE - EKSTENZIJA NAPRAVLJENA 24.11.2021.</h2><textarea readonly rows="15" cols="180" id="jsonData">${JSON.stringify(jsonArray)}</textarea>`;
 }
 
-document.querySelector('.filteri').innerHTML = "<h1 id='getJsonData' style='background: #00aeef;color: white;cursor:pointer;border-radius: 5px;text-align: center;padding: 8px;'>GET JSON DATA - KENAN MASLEŠA</h1>";
+document.querySelector('.filteri').innerHTML = "<h1 id='getJsonData' style='background: #00aeef;color: white;cursor:pointer;border-radius: 5px;text-align: center;padding: 8px;'>IZBRIŠI STARE I DODAJ NOVE ARTIKLE - KENAN MASLEŠA</h1>";
 let getJsonDataButton = document.querySelector('#getJsonData');
 getJsonDataButton.addEventListener('click', function () {
-  getArticles();
+  deleteArticles();
 })
 
 function addArticles(data){
-  fetch("https://ekupovina-803dd-default-rtdb.europe-west1.firebasedatabase.app/.json", {
+  fetch("https://ekupovina-803dd-default-rtdb.europe-west1.firebasedatabase.app/articles.json", {
     method: "POST", 
     body: JSON.stringify(data)
   }).then(res => {
@@ -50,9 +49,10 @@ function addArticles(data){
 }
 
 function deleteArticles(){
-  fetch("https://ekupovina-803dd-default-rtdb.europe-west1.firebasedatabase.app/.json", {
+  fetch("https://ekupovina-803dd-default-rtdb.europe-west1.firebasedatabase.app/articles.json", {
     method: "DELETE", 
   }).then(res => {
     console.log("Request complete! response:", res);
+    getArticles();
   });
 }
